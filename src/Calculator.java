f834b5372f26c337e3633bd7c74ad1b5acabe834
ravidro1 <ravidrrt@gmail.com>
2023-07-06 23:39:18 +0300

    finish: all functions

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,31 +8,39 @@ public class Calculator implements ActionListener {
     JTextField textField;
     JButton[] numbersButtons = new JButton[10];
     JButton[] functionButtons = new JButton[8];
-    JButton addButton,subButton,multiplyButton,divideButton;
-    JButton decimalButton,equalButton,clearButton,deleteButton;
+    JButton addButton, subButton, multiplyButton, divideButton;
+    JButton decimalButton, equalButton, clearButton, deleteButton;
     JPanel panel;
+    JPanel textPanel;
+    JLabel label;
 
-    Font font = new Font("ink free",Font.BOLD,30);
+    Font font = new Font("Arial", Font.BOLD, 30);
 
-    double num1=0,num2=0,result=0;
-    char operator;
+    double result = 0;
+    Double num1 = null, num2 = null;
+    Character operator;
 
-    public Calculator(){
+    public Calculator() {
         frame = new JFrame();
         frame.setTitle("Calculator");
-        frame.setSize(420,550);
+        frame.setSize(420, 550);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
 
         panel = new JPanel();
-        panel.setBounds(50,100,300,300);
-        panel.setLayout(new GridLayout(4,4,10 ,10));
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
 //        panel.setBackground(Color.GRAY);
 
+        label = new JLabel();
+        label.setBounds(50, -10, 300, 50);
+
+//        label.set
+
         textField = new JTextField();
-        textField.setBounds(50,25,300,50);
+        textField.setBounds(50, 25, 300, 50);
         textField.setFont(font);
         textField.setEditable(false);
 
@@ -66,10 +74,8 @@ public class Calculator implements ActionListener {
             numbersButtons[i].setFocusable(false);
         }
 
-        clearButton.setBounds(50,430,145,50);
-        deleteButton.setBounds(205,430,145,50);
-
-
+        clearButton.setBounds(50, 430, 145, 50);
+        deleteButton.setBounds(205, 430, 145, 50);
 
 
         panel.add(numbersButtons[1]);
@@ -90,6 +96,7 @@ public class Calculator implements ActionListener {
         panel.add(divideButton);
 
 
+        frame.add(label);
         frame.add(panel);
         frame.add(clearButton);
         frame.add(deleteButton);
@@ -100,17 +107,102 @@ public class Calculator implements ActionListener {
     }
 
 
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
-
         for (int i = 0; i < 10; i++)
-            if(e.getSource()==numbersButtons[i])
+            if (e.getSource() == numbersButtons[i])
                 textField.setText((textField.getText()).concat(String.valueOf(i)));
 
-//        if(e.getSource() == decimalButton)
-//            textField.setText((textField.getText()).concat(".");
+
+        if (e.getSource() == decimalButton && !textField.getText().isEmpty() && !textField.getText().contains(".")) {
+            textField.setText((textField.getText()).concat("."));
+        }
+
+
+        boolean operatorCondition = !textField.getText().isEmpty() && num1 == null;
+
+        if (e.getSource() == addButton && operatorCondition) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            label.setText(textField.getText() + " " + '+');
+            textField.setText("");
+        }
+
+        if (e.getSource() == subButton) {
+            if (textField.getText().isEmpty()) {
+                textField.setText("-");
+                return;
+            } else if (textField.getText().matches(".*[0-9].*")) {
+                num1 = Double.parseDouble(textField.getText());
+                operator = '-';
+                label.setText(textField.getText() + " " + '-');
+                textField.setText("");
+            }
+        }
+
+        if (e.getSource() == multiplyButton && operatorCondition) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            label.setText(textField.getText() + " " + '*');
+            textField.setText("");
+        }
+
+        if (e.getSource() == divideButton && operatorCondition) {
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            label.setText(textField.getText() + " " + '/');
+            textField.setText("");
+        }
+
+        if (e.getSource() == equalButton) {
+            if (!textField.getText().isEmpty())
+                num2 = Double.parseDouble(textField.getText());
+
+            if (num1 == null || num2 == null || operator == null) return;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+
+            if (result % 1 == 0)
+                textField.setText(String.valueOf((int) result));
+            else
+                textField.setText(String.valueOf(result));
+
+
+            num1 = null;
+            num2 = null;
+            operator = null;
+            label.setText("");
+        }
+
+        if (e.getSource() == clearButton) {
+            textField.setText("");
+            label.setText("");
+            num1 = null;
+            num2 = null;
+            operator = null;
+        }
+
+        if (e.getSource() == deleteButton) {
+            String textFieldData = textField.getText();
+            textField.setText("");
+
+            for (int i = 0; i < textFieldData.length() - 1; i++) {
+                textField.setText(textField.getText() + textFieldData.charAt(i));
+            }
+        }
 
     }
 
